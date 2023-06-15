6790591e93da326ee96c5f7a8eb5fb6e8081e728
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-15 22:39:00 +0700

    [HuyHieu] Change model

--- ManagerServer/Model/StatisticalDataResponse/StatisticalDataFarmQueryModel.cs
+++ ManagerServer/Model/StatisticalDataResponse/StatisticalDataFarmQueryModel.cs
@@ -1,6 +1,10 @@
-﻿namespace ManagerServer.Model.StatisticalDataResponse
+﻿using ManagerServer.Common.Enum;
+
+namespace ManagerServer.Model.StatisticalDataResponse
 {
     public class StatisticalDataFarmQueryModel
     {
+        public int FarmId { get; set; }
+        public StatisticalPeriod Period { get; set; }
     }
 }
--- ManagerServer/Model/StatisticalDataResponse/StatisticalDataZoneQueryModel.cs
+++ ManagerServer/Model/StatisticalDataResponse/StatisticalDataZoneQueryModel.cs
@@ -1,9 +1,11 @@
-﻿namespace ManagerServer.Model.StatisticalDataResponse
+﻿using ManagerServer.Common.Enum;
+
+namespace ManagerServer.Model.StatisticalDataResponse
 {
     public class StatisticalDataZoneQueryModel
     {
         public int ZoneId { get; set; }
         public int FarmId { get; set; }
-
+        public StatisticalPeriod Period { get; set; }
     }
 }
