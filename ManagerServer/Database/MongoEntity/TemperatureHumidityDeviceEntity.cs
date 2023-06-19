05c317f66d35ebfe8d943a6f20aa8d88396e1f20
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-19 22:46:45 +0700

    [HuyHieu] add entity mongo

---
+++ ManagerServer/Database/MongoEntity/TemperatureHumidityDeviceEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagerServer.Database.MongoEntity
+{
+    public class TemperatureHumidityDeviceEntity
+    {
+        public string? Id { get; set; }
+        public DateTime ValueDate { get; set; } = DateTime.Now;
+        public string TemperatureValue { get; set; } = string.Empty;
+        public string HumidityValue { get; set; } = string.Empty;
+    }
+}
