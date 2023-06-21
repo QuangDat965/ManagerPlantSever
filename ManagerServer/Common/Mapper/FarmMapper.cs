b05501cdae91d3f90f4d5d0fced0a6cb38ffae6b
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-22 01:38:22 +0700

    HuyHieu change service

--- ManagerServer/Common/Mapper/FarmMapper.cs
+++ ManagerServer/Common/Mapper/FarmMapper.cs
@@ -5,9 +5,28 @@ namespace Common.Mapper
 {
     public static class FarmMapper
     {
+        public static FarmDisplayModel FromObject(object obj)
+        {
+            var dst = new FarmDisplayModel();
+            var typ = typeof(FarmDisplayModel);
+
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                var value = property.GetValue(obj);
+                if (value != null)
+                {
+                    var p = typ.GetProperty(property.Name);
+                    if (p != null)
+                    {
+                        p.SetValue(dst, value, null);
+                    }
+                }
+            }
+            return dst;
+        }
         public static FarmDisplayModel ToFarmModel(this FarmEntity entity)
         {
-            return new FarmDisplayModel ()
+            return new FarmDisplayModel()
             {
                 Id = entity.Id,
                 Decription = entity.Description,
